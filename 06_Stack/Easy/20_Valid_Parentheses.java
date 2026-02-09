4aa3496423550252a96cf5502160442f86beac15
PreRavi_Shankar <rshankarkr6@gmail.com>
2026-02-09 13:48:17 +0530

    Stack +1 easy question inserted

---
+++ 06_Stack/Easy/20_Valid_Parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+      Stack<Character> stack = new Stack <>();
+
+      for (char ch : s.toCharArray()){
+       if (ch=='('||ch=='{'||ch=='['){
+        stack.push(ch);
+       }else{
+        if(stack.isEmpty()) return false;
+        else{
+            int top = stack.peek();
+            if(top=='{'&& ch=='}'||top=='['&& ch==']'||top=='('&& ch==')'){
+                stack.pop();
+            }else return false;
+        }
+       }
+      }
+      return stack.isEmpty();  
+    }
+}
